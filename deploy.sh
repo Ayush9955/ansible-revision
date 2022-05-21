17cbe1d910c803e03b2f67bea303cf7e7c5e6185
Ayush9955 <103412540+Ayush9955@users.noreply.github.com>
2022-05-22 00:26:16 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 deploy=$1
 if [ "deploy==$deploy" ]; then
-	ansible-playbook main.yml
+	ansible-playbook main.yml -e ansible_python_interpreter=python3
 else
 	echo please write deploy
 fi
